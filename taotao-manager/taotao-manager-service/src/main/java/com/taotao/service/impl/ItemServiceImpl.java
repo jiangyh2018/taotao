74e146af6c645cf0d4a012275dfa977b4617b54f
jiangyh2018 <1753941599@qq.com>
2018-08-09 21:29:50 +0800

    分页查询商品列表开发完成

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -1,10 +1,16 @@
 package com.taotao.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jah.common.EasyUIDataGridResult;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
 import com.taotao.service.ItemService;
 
 @Service
@@ -16,6 +22,17 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public TbItem getItemById(long id) {
 		return itemMapper.selectByPrimaryKey(id);
+	}
+
+	@Override
+	public EasyUIDataGridResult getAllItem(int pageNum,int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		List<TbItem> list=itemMapper.selectByExample(new TbItemExample());
+		System.out.println("list: "+list.size());
+		PageInfo pageInfo=new PageInfo<>(list);
+		System.out.println("list: "+list.size());
+		System.out.println("pageInfo: "+pageInfo);
+		return new EasyUIDataGridResult(pageInfo.getTotal(),pageInfo.getList());
 	}
 
 }
